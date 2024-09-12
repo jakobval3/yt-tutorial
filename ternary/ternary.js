ae81bea478e414027061d828916862fa4f870d60
tilenavs <tilenavs@gmail.com>
2024-09-12 19:18:07 +0200

    ternary

---
+++ ternary/ternary.js
@@ -0,0 +1,12 @@
+/*let age = 19;
+let message = age >= 18 ? "you're an adult" : "You're not an adult!";
+console.log(message);
+
+let time = 9;
+let greeting = time < 12 ? "good morning." : "Good afternoon.";
+console.log(greeting);
+*/
+
+let purchaseAmount = 125;
+let discount = purchaseAmount >= 100 ? 10 : 0;
+console.log(`your total is  $${purchaseAmount - purchaseAmount * (discount/100)}`);
