41c8d143f9b0cfa81085331a23398b1168128880
tilenavs <tilenavs@gmail.com>
2024-09-13 15:00:37 +0200

    switch

---
+++ switch/switch.js
@@ -0,0 +1,27 @@
+let day = 3;
+
+switch(day){
+    case 1:
+        console.log("It's monday.");
+        break;
+    case 2:
+        console.log("It's tuesday.");
+        break;
+    case 3:
+        console.log("It's wednesday.");
+        break;
+    case 4:
+        console.log("It's thursday.");
+        break;
+    case 5:
+        console.log("It's friday.");
+        break;
+    case 6:
+        console.log("It's saturday.");
+        break;
+    case 7:
+        console.log("It's sunday.");
+        break;
+    default :
+        console.log(`${day} is not a day!`);
+}
