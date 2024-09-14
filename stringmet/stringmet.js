a09b91ed673be924db52f14c7bb0b5dc22f7f9dd
tilenavs <tilenavs@gmail.com>
2024-09-14 11:33:36 +0200

    stringmet

---
+++ stringmet/stringmet.js
@@ -0,0 +1,15 @@
+/*let fullName= "Jao Brate";
+
+let firstName = fullName.slice(0, fullName.indexOf(" "));
+let lastName= fullName.slice(fullName.indexOf(" ")+1);
+
+console.log(firstName);
+console.log(lastName);*/
+
+const email= "tilenavs@gmail.com";
+
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice( email.indexOf("@")+1);
+
+console.log(username);
+console.log(extension);
